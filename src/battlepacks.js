1a93aa9c658e733700269815d82632454b76aa7f
Isaac Culver <idculver27@gmail.com>
2023-12-28 13:10:36 -0600

    table framework

--- src/battlepacks.js
+++ src/battlepacks.js
@@ -1,10 +1,13 @@
-// store data in here
-// generate table
-
-// main
 window.addEventListener("DOMContentLoaded", () => {    
     const table = document.getElementById("main");
-
-    
-
+    fetch("../battlepacks.json")
+        .then((response) => response.json())
+        .then((json) => {
+            json.sets.forEach(set => {
+                console.log(set);
+                let tr = document.createElement("tr");
+                
+                table.appendChild(tr);
+            });
+        });
 });
