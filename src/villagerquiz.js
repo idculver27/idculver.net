fc1f295d14ea683ec8622fcfd211c47496b4c43e
Isaac Culver <idculver27@gmail.com>
2024-03-03 13:20:45 -0600

    quiz framework

--- src/villagerquiz.js
+++ src/villagerquiz.js
@@ -5,6 +5,7 @@ var quizResults = {
     plains: 0,
     savanna: 0,
     snowy: 0,
+    swamp: 0,
     taiga: 0,
     // professions
     armorer: 0,
@@ -24,16 +25,100 @@ var quizResults = {
     weaponsmith: 0
 }
 
+var bank;
+var imgs;
+fetch("../villagerquiz.json")
+    .then((response) => response.json())
+    .then((json) => {
+        bank = json.bank;
+        imgs = json.imgs;
+    });
+
+var qIndex = 0;
+
 window.addEventListener("DOMContentLoaded", () => {
     btnStart.addEventListener("click", () => {
-        startQuiz();
+        displayQuestion();
     });
 });
 
-function startQuiz() {
-    btnStart.remove();
+function displayQuestion() {
+    divMain.innerHTML = "";
+
+    let p = newElement("p");
+    p.textContent = bank[qIndex].text;
+    divMain.appendChild(p);
+
+    let btn;
+    for(let answer of ["a","b","c","d"]) {
+        btn = newElement("button");
+        btn.textContent = bank[qIndex][answer].text;
+        btn.addEventListener("click", () => {
+            // add score to results
+            for(let key of bank[qIndex][answer].score) {
+                quizResults[key]++;
+            }
+
+            // next question
+            qIndex++;
+            if(qIndex < bank.length) {
+                displayQuestion();
+            } else {
+                finalResults();
+            }
+        });
+        divMain.appendChild(btn);
+    }
 }
 
-function loadQuestion() {
-    //
+function finalResults() {
+    divMain.innerHTML = "";
+
+    let biome = findWinner(["desert","jungle","plains","savanna","snowy","swamp","taiga"]);
+    let profession = findWinner(["armorer","butcher","cartographer","cleric","farmer","fisherman","fletcher","leatherworker","librarian","nitwit","stone_mason","shepherd","toolsmith","unemployed","weaponsmith"]);
+
+    let h2 = newElement("h2");
+    h2.textContent = textFormat(biome) + " " + textFormat(profession);
+    divMain.appendChild(h2);
+
+    let img = newElement("img");
+    img.src = imgs[biome][profession];
+    divMain.appendChild(img);
+}
+
+function findWinner(keys) {
+    // find top score
+    let maxScore = 0;
+    for(let key of keys) {
+        if(quizResults[key] > maxScore) {
+            maxScore = quizResults[key];
+        }
+    }
+
+    // find all keys tied for first place
+    let firstPlaceKeys = [];
+    for(let key of keys) {
+        if(quizResults[key] == maxScore) {
+            firstPlaceKeys.push(key);
+        }
+    }
+
+    // randomly select one of the top keys
+    return firstPlaceKeys[Math.floor(Math.random() * firstPlaceKeys.length)];
+}
+
+function newElement(childType, className="") {
+    let child = document.createElement(childType);
+    if(className) {
+        child.className = className;
+    }
+    return child;
+}
+
+function textFormat(raw) {
+    let words = raw.split("_");
+    for(let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+    }
+    return words.join(" ");
 }
