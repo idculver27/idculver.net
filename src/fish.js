1ab0582219ded1c64c5dd370c25283dc8d731b29
Isaac Culver <idculver27@gmail.com>
2024-05-07 20:18:11 -0500

    filters work (mostly)

---
+++ src/fish.js
@@ -0,0 +1,54 @@
+window.addEventListener("DOMContentLoaded", () => {
+    for(let input of [selectLocation,selectSeason,selectBundle]) {
+        input.addEventListener("input", () => {
+            filter();
+        });
+    }
+});
+
+function filter() {
+    // unhide all rows
+    for(let tr of fishTable.rows) {
+        tr.removeAttribute("hidden");
+    }
+
+    // location // TODO: fix rivers
+    const location = selectLocation.value;
+    if(location != "Any") {
+        for(let tr of fishTable.rows) {
+            // skip headers
+            if(tr.cells[1].textContent == "Name") continue;
+            // hide non-matching locations
+            if(!tr.cells[3].textContent.includes(location)) {
+                tr.setAttribute("hidden",true);
+            }
+        }
+    }
+
+    // season
+    const season = selectSeason.value;
+    if(season != "Any") {
+        for(let tr of fishTable.rows) {
+            // skip headers
+            if(tr.cells[1].textContent == "Name") continue;
+            // hide non-matching seasons
+            if(tr.cells[4].textContent == "Any") continue;
+            if(!tr.cells[4].textContent.includes(season)) {
+                tr.setAttribute("hidden",true);
+            }
+        }
+    }
+
+    // bundle
+    const bundle = selectBundle.value;
+    if(bundle != "Any") {
+        for(let tr of fishTable.rows) {
+            // skip headers
+            if(tr.children[1].textContent == "Name") continue;
+            // hide non-matching bundles
+            if(!tr.children[7].textContent.includes(bundle)) {
+                tr.setAttribute("hidden",true);
+            }
+        }
+    }
+}
