26837a4f715b2106a6d5120d6cdec0b846845394
Isaac Culver <idculver27@gmail.com>
2024-07-30 22:03:53 -0500

    Revert "crops wip"
    This reverts commit ee9009eb0312458d358c9339b6f933876c438436.

--- src/fish.js
+++ src/fish.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
     buildTable();
 
-    for(let input of [selectSeason, onlyBundles]) {
+    for(let input of [selectSeason,onlyBundles]) {
         input.addEventListener("input", () => {
             filter();
         });
@@ -15,26 +15,26 @@ function buildTable() {
             json.forEach(fish => {
                 let tr = document.createElement("tr");
                 tr.innerHTML = `
-                    <td><img src="images/fish/${fish.name.replace(" ", "_")}.png"></td>
+                    <td><img src="images/fish/${fish.name.replace(" ","_")}.png"></td>
                     <td>${fish.name}</td>
                     <td style="max-width: 300px;">${fish.description}</td>
                     <td style="text-align: right;">
                         ${fish.price}g<br>
-                        <img src="images/fish/Silver_Quality.png"> ${Math.floor(fish.price * 1.25)}g<br>
-                        <img src="images/fish/Gold_Quality.png"> ${Math.floor(fish.price * 1.5)}g<br>
-                        <img src="images/fish/Iridium_Quality.png"> ${fish.price * 2}g
+                        <img src="images/fish/Silver_Quality.png"> ${Math.floor(fish.price*1.25)}g<br>
+                        <img src="images/fish/Gold_Quality.png"> ${Math.floor(fish.price*1.5)}g<br>
+                        <img src="images/fish/Iridium_Quality.png"> ${fish.price*2}g
                     </td>
                     <td style="text-align: right;">
-                        ${Math.floor(fish.price * 1.25)}g<br>
-                        <img src="images/fish/Silver_Quality.png"> ${Math.floor(Math.floor(fish.price * 1.25) * 1.25)}g<br>
-                        <img src="images/fish/Gold_Quality.png"> ${Math.floor(Math.floor(fish.price * 1.5) * 1.25)}g<br>
-                        <img src="images/fish/Iridium_Quality.png"> ${Math.floor(fish.price * 2 * 1.25)}g
+                        ${Math.floor(fish.price*1.25)}g<br>
+                        <img src="images/fish/Silver_Quality.png"> ${Math.floor(Math.floor(fish.price*1.25)*1.25)}g<br>
+                        <img src="images/fish/Gold_Quality.png"> ${Math.floor(Math.floor(fish.price*1.5)*1.25)}g<br>
+                        <img src="images/fish/Iridium_Quality.png"> ${Math.floor(fish.price*2*1.25)}g
                     </td>
                     <td style="text-align: right;">
-                        ${Math.floor(fish.price * 1.5)}g<br>
-                        <img src="images/fish/Silver_Quality.png"> ${Math.floor(Math.floor(fish.price * 1.25) * 1.5)}g<br>
-                        <img src="images/fish/Gold_Quality.png"> ${Math.floor(Math.floor(fish.price * 1.5) * 1.5)}g<br>
-                        <img src="images/fish/Iridium_Quality.png"> ${Math.floor(fish.price * 2 * 1.5)}g
+                        ${Math.floor(fish.price*1.5)}g<br>
+                        <img src="images/fish/Silver_Quality.png"> ${Math.floor(Math.floor(fish.price*1.25)*1.5)}g<br>
+                        <img src="images/fish/Gold_Quality.png"> ${Math.floor(Math.floor(fish.price*1.5)*1.5)}g<br>
+                        <img src="images/fish/Iridium_Quality.png"> ${Math.floor(fish.price*2*1.5)}g
                     </td>
                     <td style="max-width: 180px;">${fish.locations.join("<br>")}</td>
                     <td style="max-width: 180px;">${fish.seasons.join("<br>")}</td>
@@ -44,7 +44,7 @@ function buildTable() {
                     <td>${fish.behavior}</td>
                     <td>${fish.xp}</td>
                     <td>${fish.uses.join("<br>")}</td>
-                `;
+                `
                 fishTable.appendChild(tr);
             });
         });
@@ -66,7 +66,7 @@ function filter() {
             if(tr.cells[7].textContent == "") continue;
             if(tr.cells[7].textContent.includes("Any")) continue;
             if(!tr.cells[7].textContent.includes(season)) {
-                tr.setAttribute("hidden", true);
+                tr.setAttribute("hidden",true);
             }
         }
     }
@@ -78,7 +78,7 @@ function filter() {
             if(tr.children[1].textContent == "Name") continue;
             // hide non-matching bundles
             if(!tr.children[13].textContent.includes("Bundle")) {
-                tr.setAttribute("hidden", true);
+                tr.setAttribute("hidden",true);
             }
         }
     }
