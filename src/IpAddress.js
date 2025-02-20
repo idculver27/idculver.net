04a8480a4279d8eeb44233d45c8a0f6b7244681e
Isaac Culver <idculver27@gmail.com>
2025-02-19 18:02:56 -0600

    add more ip ranges

--- src/IpAddress.js
+++ src/IpAddress.js
@@ -139,16 +139,26 @@ export default class IpAddress {
 		const ranges = [
 			{ start: "0.0.0.0", end: "9.255.255.255", type: "Public" },
 			{ start: "10.0.0.0", end: "10.255.255.255", type: "Private (10.0.0.0/8)" },
-			{ start: "11.0.0.0", end: "126.255.255.255", type: "Public" },
+			{ start: "11.0.0.0", end: "100.63.255.255", type: "Public" },
+			{ start: "100.64.0.0", end: "100.127.255.255", type: "CGNAT (100.64.0.0/10)" },
+			{ start: "100.128.0.0", end: "126.255.255.255", type: "Public" },
 			{ start: "127.0.0.0", end: "127.255.255.255", type: "Loopback (127.0.0.0/8)" },
 			{ start: "128.0.0.0", end: "169.253.255.255", type: "Public" },
 			{ start: "169.254.0.0", end: "169.254.255.255", type: "Link-Local (169.254.0.0/16)" },
 			{ start: "169.255.0.0", end: "172.15.255.255", type: "Public" },
 			{ start: "172.16.0.0", end: "172.31.255.255", type: "Private (172.16.0.0/12)" },
-			{ start: "172.32.0.0", end: "192.167.255.255", type: "Public" },
+			{ start: "172.32.0.0", end: "192.0.1.255", type: "Public" },
+			{ start: "192.0.2.0", end: "192.0.2.255", type: "Documentation (192.0.2.0/24)" },
+			{ start: "192.0.3.0", end: "192.167.255.255", type: "Public" },
 			{ start: "192.168.0.0", end: "192.168.255.255", type: "Private (192.168.0.0/16)" },
-			{ start: "192.169.0.0", end: "223.255.255.255", type: "Public" },
-			{ start: "224.0.0.0", end: "239.255.255.255", type: "Multicast (224.0.0.0/4)" },
+			{ start: "192.169.0.0", end: "198.51.99.255", type: "Public" },
+			{ start: "198.51.100.0", end: "198.51.100.255", type: "Documentation (198.51.100.0/24)" },
+			{ start: "198.51.101.0", end: "203.0.112.255", type: "Public" },
+			{ start: "203.0.113.0", end: "203.0.113.255", type: "Documentation (203.0.113.0/24)" },
+			{ start: "203.0.114.0", end: "223.255.255.255", type: "Public" },
+			{ start: "224.0.0.0", end: "233.251.255.255", type: "Multicast (224.0.0.0/4)" },
+			{ start: "233.252.0.0", end: "233.252.0.255", type: "Documentation (233.252.0.0/24)" },
+			{ start: "233.252.1.0", end: "239.255.255.255", type: "Multicast (224.0.0.0/4)" },
 			{ start: "240.0.0.0", end: "255.255.255.255", type: "Reserved (240.0.0.0/4)" }
 		]
 		let thisStart = parseInt(IpAddress.dec2bin(this.networkAddress), 2);
@@ -176,7 +186,7 @@ export default class IpAddress {
 	static dec2bin(dec) {
 		let bin = "";
 		dec.split(".").forEach((octet) => {
-			bin += parseInt(octet).toString(2).padStart(8, "0"); // for i?
+			bin += parseInt(octet).toString(2).padStart(8, "0");
 		});
 		return bin;
 	}
