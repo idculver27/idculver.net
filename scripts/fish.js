b2e506c2136f2bd6a13e35fe80f74054b9d29dd5
Isaac Culver <idculver27@gmail.com>
2025-04-21 23:52:38 -0500

    add all normal fish

--- scripts/fish.js
+++ scripts/fish.js
@@ -1,5 +1,13 @@
 window.addEventListener("DOMContentLoaded", () => {
 	buildTableOcean();
+	buildTableForestPond();
+	buildTableMountainLake();
+	buildTableSecretWoodsPond();
+	buildTableForestWaterfall();
+	buildTableRiver();
+	buildTableMines();
+	buildTableGingerIsland();
+	buildTableSpecial();
 });
 
 function buildTableOcean() {
@@ -8,14 +16,241 @@ function buildTableOcean() {
 		.then((json) => {
 			json["Ocean"].forEach(fish => {
 				let tr = document.createElement("tr");
+
+				// fish
 				tr.innerHTML = `<td><img src="images/fish/${fish.fish_name.replace(" ", "_")}.png"><span>${fish.fish_name}</span></td>`;
+				// base price
 				tr.innerHTML += `<td style="text-align: right;">${fish.base_price}g</td>`;
-				tr.innerHTML += `<td>${fish.season}</td>`
-				tr.innerHTML += `<td>${fish.weather}</td>`
-				tr.innerHTML += `<td>${fish.time}</td>`
-				if (fish.bundle_name) tr.innerHTML += `<td class="centered"><img src="images/fish/${fish.bundle_name.replaceAll(" ", "_")}.png" title="${fish.bundle_name}"></td>`;
+				// season
+				if (!fish.season) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.season}</td>`;
+				// weather
+				if (!fish.weather) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.weather}</td>`;
+				// time
+				if (!fish.time) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.time}</td>`;
+				// bundle
+				if (fish.bundle) tr.innerHTML += `<td class="centered"><img src="images/fish/${fish.bundle.replaceAll(" ", "_")}.png" title="${fish.bundle}"></td>`;
 				else tr.innerHTML += "<td></td>";
+
 				tableOcean.appendChild(tr);
+			});
+		});
+}
+
+function buildTableForestPond() {
+	fetch("../databases/fish.json")
+		.then((response) => response.json())
+		.then((json) => {
+			json["Forest Pond"].forEach(fish => {
+				let tr = document.createElement("tr");
+
+				// fish
+				tr.innerHTML = `<td><img src="images/fish/${fish.fish_name.replace(" ", "_")}.png"><span>${fish.fish_name}</span></td>`;
+				// base price
+				tr.innerHTML += `<td style="text-align: right;">${fish.base_price}g</td>`;
+				// season
+				if (!fish.season) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.season}</td>`;
+				// weather
+				if (!fish.weather) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.weather}</td>`;
+				// time
+				if (!fish.time) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.time}</td>`;
+				// bundle
+				if (fish.bundle) tr.innerHTML += `<td class="centered"><img src="images/fish/${fish.bundle.replaceAll(" ", "_")}.png" title="${fish.bundle}"></td>`;
+				else tr.innerHTML += "<td></td>";
+
+				tableForestPond.appendChild(tr);
+			});
+		});
+}
+
+function buildTableMountainLake() {
+	fetch("../databases/fish.json")
+		.then((response) => response.json())
+		.then((json) => {
+			json["Mountain Lake"].forEach(fish => {
+				let tr = document.createElement("tr");
+
+				// fish
+				tr.innerHTML = `<td><img src="images/fish/${fish.fish_name.replace(" ", "_")}.png"><span>${fish.fish_name}</span></td>`;
+				// base price
+				tr.innerHTML += `<td style="text-align: right;">${fish.base_price}g</td>`;
+				// season
+				if (!fish.season) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.season}</td>`;
+				// weather
+				if (!fish.weather) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.weather}</td>`;
+				// time
+				if (!fish.time) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.time}</td>`;
+				// bundle
+				if (fish.bundle) tr.innerHTML += `<td class="centered"><img src="images/fish/${fish.bundle.replaceAll(" ", "_")}.png" title="${fish.bundle}"></td>`;
+				else tr.innerHTML += "<td></td>";
+
+				tableMountainLake.appendChild(tr);
+			});
+		});
+}
+
+function buildTableSecretWoodsPond() {
+	fetch("../databases/fish.json")
+		.then((response) => response.json())
+		.then((json) => {
+			json["Secret Woods Pond"].forEach(fish => {
+				let tr = document.createElement("tr");
+
+				// fish
+				tr.innerHTML = `<td><img src="images/fish/${fish.fish_name.replace(" ", "_")}.png"><span>${fish.fish_name}</span></td>`;
+				// base price
+				tr.innerHTML += `<td style="text-align: right;">${fish.base_price}g</td>`;
+				// season
+				if (!fish.season) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.season}</td>`;
+				// weather
+				if (!fish.weather) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.weather}</td>`;
+				// time
+				if (!fish.time) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.time}</td>`;
+				// bundle
+				if (fish.bundle) tr.innerHTML += `<td class="centered"><img src="images/fish/${fish.bundle.replaceAll(" ", "_")}.png" title="${fish.bundle}"></td>`;
+				else tr.innerHTML += "<td></td>";
+
+				tableSecretWoodsPond.appendChild(tr);
+			});
+		});
+}
+
+function buildTableForestWaterfall() {
+	fetch("../databases/fish.json")
+		.then((response) => response.json())
+		.then((json) => {
+			json["Forest Waterfall"].forEach(fish => {
+				let tr = document.createElement("tr");
+
+				// fish
+				tr.innerHTML = `<td><img src="images/fish/${fish.fish_name.replace(" ", "_")}.png"><span>${fish.fish_name}</span></td>`;
+				// base price
+				tr.innerHTML += `<td style="text-align: right;">${fish.base_price}g</td>`;
+				// season
+				if (!fish.season) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.season}</td>`;
+
+				tableForestWaterfall.appendChild(tr);
+			});
+		});
+}
+
+function buildTableRiver() {
+	fetch("../databases/fish.json")
+		.then((response) => response.json())
+		.then((json) => {
+			json["River"].forEach(fish => {
+				let tr = document.createElement("tr");
+
+				// fish
+				tr.innerHTML = `<td><img src="images/fish/${fish.fish_name.replace(" ", "_")}.png"><span>${fish.fish_name}</span></td>`;
+				// base price
+				tr.innerHTML += `<td style="text-align: right;">${fish.base_price}g</td>`;
+				// season
+				if (!fish.season) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.season}</td>`;
+				// weather
+				if (!fish.weather) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.weather}</td>`;
+				// time
+				if (!fish.time) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.time}</td>`;
+				// location
+				tr.innerHTML += `<td>${fish.location}</td>`;
+				// bundle
+				if (fish.bundle) tr.innerHTML += `<td class="centered"><img src="images/fish/${fish.bundle.replaceAll(" ", "_")}.png" title="${fish.bundle}"></td>`;
+				else tr.innerHTML += "<td></td>";
+
+				tableRiver.appendChild(tr);
+			});
+		});
+}
+
+function buildTableMines() {
+	fetch("../databases/fish.json")
+		.then((response) => response.json())
+		.then((json) => {
+			json["Mines"].forEach(fish => {
+				let tr = document.createElement("tr");
+
+				// fish
+				tr.innerHTML = `<td><img src="images/fish/${fish.fish_name.replace(" ", "_")}.png"><span>${fish.fish_name}</span></td>`;
+				// base price
+				tr.innerHTML += `<td style="text-align: right;">${fish.base_price}g</td>`;
+				// level
+				tr.innerHTML += `<td>${fish.level}</td>`;
+				// bundle
+				if (fish.bundle) tr.innerHTML += `<td class="centered"><img src="images/fish/${fish.bundle.replaceAll(" ", "_")}.png" title="${fish.bundle}"></td>`;
+				else tr.innerHTML += "<td></td>";
+
+				tableMines.appendChild(tr);
+			});
+		});
+}
+
+function buildTableGingerIsland() {
+	fetch("../databases/fish.json")
+		.then((response) => response.json())
+		.then((json) => {
+			json["Ginger Island"].forEach(fish => {
+				let tr = document.createElement("tr");
+
+				// fish
+				tr.innerHTML = `<td><img src="images/fish/${fish.fish_name.replace(" ", "_")}.png"><span>${fish.fish_name}</span></td>`;
+				// base price
+				tr.innerHTML += `<td style="text-align: right;">${fish.base_price}g</td>`;
+				// weather
+				if (!fish.weather) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.weather}</td>`;
+				// time
+				if (!fish.time) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.time}</td>`;
+				// location
+				tr.innerHTML += `<td>${fish.location}</td>`;
+				// bundle
+				if (fish.bundle) tr.innerHTML += `<td class="centered"><img src="images/fish/${fish.bundle.replaceAll(" ", "_")}.png" title="${fish.bundle}"></td>`;
+				else tr.innerHTML += "<td></td>";
+
+				tableGingerIsland.appendChild(tr);
+			});
+		});
+}
+
+function buildTableSpecial() {
+	fetch("../databases/fish.json")
+		.then((response) => response.json())
+		.then((json) => {
+			json["Special"].forEach(fish => {
+				let tr = document.createElement("tr");
+
+				// fish
+				tr.innerHTML = `<td><img src="images/fish/${fish.fish_name.replace(" ", "_")}.png"><span>${fish.fish_name}</span></td>`;
+				// base price
+				tr.innerHTML += `<td style="text-align: right;">${fish.base_price}g</td>`;
+				// weather
+				if (!fish.weather) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.weather}</td>`;
+				// time
+				if (!fish.time) tr.innerHTML += "<td>Any</td>";
+				else tr.innerHTML += `<td>${fish.time}</td>`;
+				// location
+				tr.innerHTML += `<td>${fish.location}</td>`;
+				// bundle
+				if (fish.bundle) tr.innerHTML += `<td class="centered"><img src="images/fish/${fish.bundle.replaceAll(" ", "_")}.png" title="${fish.bundle}"></td>`;
+				else tr.innerHTML += "<td></td>";
+
+				tableSpecial.appendChild(tr);
 			});
 		});
 }
